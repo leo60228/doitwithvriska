677f6534dced07f074569a5f0b3739b009774b0c
leo60228 <leo@60228.dev>
2021-01-23 10:37:47 -0500

    2captcha

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from faker import Faker
+from dotenv import load_dotenv
+from twocaptcha import TwoCaptcha
+import os
+
+load_dotenv()
+
+apikey = os.getenv("2CAPTCHA_APIKEY")
+sitekey = "6LclNi8aAAAAAELxL1QpqA4ymERKv1MV7AFGHfbN"
+url = "https://www.doitwithoutdues.com/contact"
+
+solver = TwoCaptcha(apiKey=apikey)
 
 fake = Faker()
 Faker.seed()
 
-print(fake.ascii_email())
+
+def contact(fake, solver, sitekey, url):
+    result = solver.recaptcha(sitekey=sitekey, url=url)
+    print(result)
+
+
+contact(fake, solver, sitekey, url)
