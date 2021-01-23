585d757b716ebc9c62863342729ef0543b5865fb
leo60228 <leo@60228.dev>
2021-01-23 10:47:55 -0500

    Get key

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from faker import Faker
 from dotenv import load_dotenv
 from twocaptcha import TwoCaptcha
 import os
+import requests
+from argparse import ArgumentParser
+from time import sleep
 
 load_dotenv()
 
@@ -15,9 +18,35 @@ fake = Faker()
 Faker.seed()
 
 
-def contact(fake, solver, sitekey, url):
-    result = solver.recaptcha(sitekey=sitekey, url=url)
-    print(result)
+def contact(fake, solver, sitekey, url, testing=True):
+    print("Solving captcha...")
+
+    key = "[KEY]"
+    if not testing:
+        key_req = requests.post(
+            "https://www.doitwithoutdues.com/api/form/FormSubmissionKey", data={}
+        )
+        key_res = key_req.json()
+        key = key_res["key"]
+    print("Got key:", key)
+
+    captcha_code = "[CAPTCHA]"
+    if not testing:
+        captcha_res = solver.recaptcha(sitekey=sitekey, url=url)
+        captcha_code = captcha_res.code
+    print("Got code:", captcha_code)
 
 
-contact(fake, solver, sitekey, url)
+parser = ArgumentParser()
+parser.add_argument("--real", action="store_true")
+args = parser.parse_args()
+
+if args.real:
+    while True:
+        try:
+            contact(fake, solver, sitekey, url, False)
+        except:
+            print("Error:", sys.exc_info()[0])
+            sleep(1)
+else:
+    contact(fake, solver, sitekey, url, True)
